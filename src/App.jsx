9f00b68f903c0b23479b737a091eebad507501e9
Sparsh Gupta <sparshgupta78970@gmail.com>
2026-01-31 20:27:16 +0530

    Add homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
+import Home from "./components/Home"
 import Product from "./components/Product"
 
 function App() {
 
   return (
     <>
-      <h1
-        className="text-3xl font-bold underline text-cyan-500"
-      >
-        Hello Vite + React!
-      </h1>
+      <Home />
       
       {/* Razorpahy test: For demo of working */}
       {/* <Product/>   */}
